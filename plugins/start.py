771957e38acfa4ab99d2ea9c6de02391d9ef6563
Personal Robot <86073822+Agoriadmin@users.noreply.github.com>
2021-09-05 17:10:50 +0530

    Update and rename start.py to start.py

--- plugins/start.py
+++ plugins/start.py
@@ -24,7 +24,7 @@ async def start(client, message):
                 ],
                 [
                     InlineKeyboardButton(
-                        "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")
+                        "💝 JOIN OUR CHANNEL 💝", url="https://t.me/Agorimovies")
                 ]
             ]
         ),
@@ -45,7 +45,7 @@ async def help(client, message):
                 ],
                 [
                     InlineKeyboardButton(
-                        "⭕️ SUPPORT ⭕️", url="https://t.me/TroJanzSupport")
+                        "⭕️ SUPPORT ⭕️", url="https://t.me/agorihome")
                 ]
             ]
         ),
@@ -66,7 +66,7 @@ async def about(client, message):
                 ],
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Streams-Extractor")
+                        "Tronjanz CODE", url="https://github.com/TroJanzHEX/Streams-Extractor")
                 ]
             ]
         ),
